4c64d5b977aa4697ae34ac25be7ff7cfc87f2d33
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-07-14 16:08:38 +0530

    feat: typesafe environment variables

---
+++ packages/env/src/client/index.ts
@@ -0,0 +1,10 @@
+import { createEnv } from "@t3-oss/env-core"
+import { z } from "zod"
+
+export const clientEnv = createEnv({
+  clientPrefix: "NEXT_PUBLIC_",
+  client: {
+    NEXT_PUBLIC_BACKEND_URL: z.string().default("http://localhost:8080"),
+  },
+  runtimeEnv: process.env,
+})
---
+++ packages/env/src/server/index.ts
@@ -0,0 +1,65 @@
+import { createEnv } from "@t3-oss/env-core"
+import { z } from "zod"
+
+export const serverEnv = createEnv({
+  server: {
+    NODE_ENV: z
+      .enum(["development", "production", "test"])
+      .default("development"),
+
+    LOG_LEVEL: z
+      .enum(["info", "error", "fatal", "warn", "debug", "trace", "silent"])
+      .default("info"),
+
+    PORT: z.coerce.number().default(8080),
+
+    HOST: z.string().default("localhost"),
+
+    DATABASE_URL: z.string({
+      required_error: "DATABASE_URL is required in environment variables",
+    }),
+
+    BETTER_AUTH_URL: z.string().default("http://localhost:3000"),
+
+    BETTER_AUTH_SECRET: z.string({
+      required_error: "BETTER_AUTH_SECRET is required in environment variables",
+    }),
+
+    AWS_ACCESS_KEY_ID: z.string({
+      required_error: "AWS_ACCESS_KEY_ID is required in environment variables",
+    }),
+
+    AWS_SECRET_ACCESS_KEY: z.string({
+      required_error:
+        "AWS_SECRET_ACCESS_KEY is required in environment variables",
+    }),
+
+    AWS_REGION: z.string({
+      required_error: "AWS_REGION is required in environment variables",
+    }),
+
+    S3_UPLOAD_BUCKET: z.string({
+      required_error: "S3_UPLOAD_BUCKET is required in environment variables",
+    }),
+
+    UPSTASH_REDIS_REST_URL: z.string({
+      required_error:
+        "UPSTASH_REDIS_REST_URL is required in environment variables",
+    }),
+
+    UPSTASH_REDIS_REST_TOKEN: z.string({
+      required_error:
+        "UPSTASH_REDIS_REST_TOKEN is required in environment variables",
+    }),
+
+    STRIPE_SECRET_KEY: z.string({
+      required_error: "STRIPE_SECRET_KEY is required in environment variables",
+    }),
+
+    STRIPE_WEBHOOK: z.string({
+      required_error: "STRIPE_WEBHOOK is required in environment variables",
+    }),
+  },
+
+  runtimeEnv: process.env,
+})
