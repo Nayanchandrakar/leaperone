1aa861b22454888215db552f4d4cb03fad6dc8e5
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-08-10 16:01:43 +0530

    chore: added input variations

--- packages/ui/src/components/input.tsx
+++ packages/ui/src/components/input.tsx
@@ -1,20 +1,41 @@
 import { cn } from "@leapercrm/ui/lib/utils"
+import { cva, type VariantProps } from "class-variance-authority"
 import type * as React from "react"
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+const inputVariants = cva(
+  "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex  w-full min-w-0 shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm aria-invalid:ring-destructive/20 aria-invalid:border-destructive",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-transparent border border-input focus-visible:border-zinc-300",
+        gray: "bg-neutral-100 border border-zinc-100 focus-visible:border-zinc-200",
+      },
+      size: {
+        default: "h-10 rounded-xl px-3 py-1 text-base",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  },
+)
+
+function Input({
+  className,
+  type,
+  variant,
+  ...props
+}: React.ComponentProps<"input"> & VariantProps<typeof inputVariants>) {
   return (
     <input
       type={type}
       data-slot="input"
-      className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className,
-      )}
+      className={cn(inputVariants({ variant, className }))}
       {...props}
     />
   )
 }
 
-export { Input }
+export { Input, inputVariants }
