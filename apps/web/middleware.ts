c65ee093074be98a3de9464ec20e2ec6531d14ea
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-08-27 19:33:22 +0530

    chore: turn off middleware redirect for now

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,17 +1,20 @@
-import { createNEMO, type MiddlewareConfig } from "@rescale/nemo"
-import { globalMiddlewares } from "@/app/_middleware"
-import { auth } from "@/app/(auth)/_middleware"
+// import { createNEMO, type MiddlewareConfig } from "@rescale/nemo"
 
-const middlewares: MiddlewareConfig = {
-  "/(login|sign-up|forgot-password)": auth,
-}
+// import { globalMiddlewares } from "@/app/_middleware"
+// import { auth } from "@/app/(auth)/_middleware"
 
-export const middleware = createNEMO(middlewares, globalMiddlewares, {
-  debug: true,
-  silent: true,
-  enableTiming: true,
-})
+// export const middlewares: MiddlewareConfig = {
+// "/(login|sign-up|forgot-password)": auth,
+// }
 
-export const config = {
-  matcher: ["/((?!_next/|_static|_vercel|[\\w-]+\\.\\w+).*)"],
-}
+// export const middleware = createNEMO(middlewares, globalMiddlewares, {
+//   debug: true,
+//   silent: true,
+//   enableTiming: true,
+// })
+
+// export const config = {
+//   matcher: ["/((?!_next/|_static|_vercel|[\\w-]+\\.\\w+).*)"],
+// }
+
+export const middleware = () => {}
