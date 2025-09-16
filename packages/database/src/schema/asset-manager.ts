deb1ebf0fd4a166f7b079285fd1e68a2e61e8bbd
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-09-16 18:11:27 +0530

    feat: added final asset manager database schema

--- packages/database/src/schema/asset-manager.ts
+++ packages/database/src/schema/asset-manager.ts
@@ -4,7 +4,7 @@ import { bigint, pgTable, text } from "drizzle-orm/pg-core"
 import { timestamps } from "../utils"
 import { workspace } from "./workspace"
 
-export const storage = pgTable("storage", {
+export const assetManager = pgTable("asset_manager", {
   id: text()
     .primaryKey()
     .$defaultFn(() => createId()),
@@ -12,7 +12,7 @@ export const storage = pgTable("storage", {
     .references(() => workspace.id, { onDelete: "cascade" })
     .unique()
     .notNull(),
-  quota: bigint({ mode: "number" }).notNull().default(STORAGE_QUOTA),
-  usage: bigint({ mode: "number" }).notNull().default(0),
+  quota: bigint({ mode: "number" }).default(STORAGE_QUOTA).notNull(),
+  usage: bigint({ mode: "number" }).default(0).notNull(),
   ...timestamps,
 })
--- packages/database/src/schema/assets.ts
+++ packages/database/src/schema/assets.ts
@@ -1,20 +1,19 @@
 import { createId } from "@paralleldrive/cuid2"
 import { integer, pgTable, text } from "drizzle-orm/pg-core"
 import { timestamps } from "../utils"
-import { storage } from "./storage"
+import { assetManager } from "./asset-manager"
 
 export const asset = pgTable("asset", {
   id: text()
     .primaryKey()
     .$defaultFn(() => createId()),
-  storageId: text()
-    .references(() => storage.id, { onDelete: "cascade" })
+  assetManagerId: text()
+    .references(() => assetManager.id, { onDelete: "cascade" })
     .notNull(),
-  fileName: text().notNull(),
+  name: text().notNull(),
   mime: text().notNull(),
-  size: integer().notNull().default(0),
   ext: text().notNull(),
-  storageKey: text().notNull(),
-  url: text().notNull(),
+  key: text().notNull(),
+  size: integer().default(0).notNull(),
   ...timestamps,
 })
