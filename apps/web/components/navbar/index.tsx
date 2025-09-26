843be1a122c31ee1d333090f38a49d79dc0bb11f
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-09-26 13:32:04 +0530

    feat: navbar typography improve

--- apps/web/components/navbar/index.tsx
+++ apps/web/components/navbar/index.tsx
@@ -1,19 +1,15 @@
 import { getSession } from "@/actions/global/get-session"
 import { Logo } from "@/components/navbar/logo"
 import { MainNav } from "@/components/navbar/main-nav"
-import { MobileNav } from "@/components/navbar/mobile-nav"
-import { Container } from "@/components/shared/container"
 
 export const Navbar = async () => {
   const session = await getSession()
-
   return (
-    <header className="bg-primary sticky top-0 z-50 h-16">
-      <Container className=" flex items-center justify-between">
+    <header className="bg-primary sticky top-0 z-50">
+      <div className="container flex items-center justify-between">
         <Logo />
         <MainNav session={session} />
-        <MobileNav />
-      </Container>
+      </div>
     </header>
   )
 }
