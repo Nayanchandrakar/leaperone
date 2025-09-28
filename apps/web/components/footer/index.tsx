60b14c858aed1269063c9a15faef636a0c94f4d4
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-09-28 13:41:24 +0530

    Revert "feat: marketing footer typography improvment"
    This reverts commit a923841b0f1406505d05e0e211780a8ed5d130bc.

--- apps/web/components/footer/index.tsx
+++ apps/web/components/footer/index.tsx
@@ -3,24 +3,25 @@
 import { useMemo } from "react"
 import { FooterDescription } from "@/components/footer/footer-description"
 import { FooterNavigation } from "@/components/footer/footer-navigation"
+import { Container } from "@/components/shared/container"
 
 export const Footer = () => {
   const currentYear = useMemo(() => new Date().getFullYear(), [])
 
   return (
-    <footer className="bg-muted">
-      <div className="container">
-        <div className="grid grid-cols-2 gap-x-14 gap-y-10 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 py-8 sm:py-12">
+    <footer className="border-t border-border bg-muted">
+      <Container>
+        <div className="grid grid-cols-2 gap-x-14 gap-y-10 px-5 sm:px-6 py-8 sm:py-12 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 xl:px-0">
           <FooterDescription />
           <FooterNavigation />
         </div>
 
-        <div className="flex items-center justify-center border-t border-border p-4">
-          <p className="text-foreground text-xs leading-loose sm:text-sm">
+        <div className="flex items-center justify-center border-t border-border p-6">
+          <p className="text-center font-normal text-muted-foreground sm:text-base text-sm">
             &copy; {currentYear} Leaper CRM. All Rights Reserved.
           </p>
         </div>
-      </div>
+      </Container>
     </footer>
   )
 }
