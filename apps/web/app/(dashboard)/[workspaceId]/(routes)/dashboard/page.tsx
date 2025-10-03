89d394a4af91057eb4e2ad5a343f5868558f88e5
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-10-03 21:42:43 +0530

    added: basic routing structure for dashboard , teams ,analytics page

--- apps/web/app/(dashboard)/[workspaceId]/(routes)/dashboard/page.tsx
+++ apps/web/app/(dashboard)/[workspaceId]/(routes)/dashboard/page.tsx
@@ -1,31 +1,9 @@
-import { hasPermissions } from "@app/database/repository/role-permission"
-import { isMemberOfWorkspace } from "@app/database/repository/workspace-member"
-import { redirect } from "next/navigation"
-import { getSession } from "@/actions/global/get-session"
-
-type IDashboardPage = {
+interface Props {
   params: Promise<{ workspaceId: string }>
 }
 
-export default async function DashboardPage({ params }: IDashboardPage) {
-  const [session, { workspaceId }] = await Promise.all([getSession(), params])
-
-  if (!session) redirect("/login")
-
-  const workspaceMembers = await isMemberOfWorkspace(
-    session.user.id,
-    workspaceId,
-  )
-
-  if (!workspaceMembers) redirect("/not-found")
-
-  const canAccess = await hasPermissions(
-    session.user.id,
-    workspaceMembers.roleId,
-    ["manage:members"],
-  )
-
-  if (!canAccess) redirect("/some-error-page")
+export default async function DashboardPage({ params }: Props) {
+  const { workspaceId } = await params
 
   return <div className="">Dashboard page {workspaceId}</div>
 }
