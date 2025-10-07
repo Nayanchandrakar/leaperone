ef19a1c75694e3c1309753fb4f17b749e89cff76
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-10-07 22:35:31 +0530

    added: responsive in dashboard-footer.tsx

--- apps/web/features/dashboard/components/ui/dashboard-footer.tsx
+++ apps/web/features/dashboard/components/ui/dashboard-footer.tsx
@@ -5,19 +5,19 @@ export const DashboardFooter = () => {
 
   return (
     <footer className="border-t border-border">
-      <div className="h-14 flex items-center justify-center">
-        <nav className="flex items-center justify-center text-sm text-muted-foreground font-normal divide-x divide-border">
-          <p className="px-4">
+      <div className="w-full p-4">
+        <nav className="flex flex-col md:flex-row items-center justify-center text-xs md:text-sm text-muted-foreground font-normal gap-1 md:gap-0 divide-y md:divide-y-0 md:divide-x divide-border text-center">
+          <p className="px-0 py-1 md:px-4 md:py-0">
             &copy; {currentYear} Leaper One. All Rights Reserved.
           </p>
           <Link
-            className="px-4 hover:text-primary rounded-sm"
+            className="px-0 py-1 md:px-4 md:py-0 hover:text-primary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring rounded-sm"
             href="/privacy-policy"
           >
             Privacy Policy
           </Link>
           <Link
-            className="px-4 hover:text-primary"
+            className="px-0 py-1 md:px-4 md:py-0 hover:text-primary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring rounded-sm"
             href="/terms-and-conditions"
           >
             Terms and Conditions
