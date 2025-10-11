52b3381fddcf655f3e913dc9567f71b5daad4075
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-10-11 10:34:18 +0530

    feat: added some extra http methods for sandbox server

--- apps/sandbox/src/app/[[...route]]/route.ts
+++ apps/sandbox/src/app/[[...route]]/route.ts
@@ -5,3 +5,6 @@ const sandbox = bootStrap()
 
 export const GET = handle(sandbox)
 export const POST = handle(sandbox)
+export const OPTIONS = handle(sandbox)
+export const PUT = handle(sandbox)
+export const PATCH = handle(sandbox)
