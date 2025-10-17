239b0d17d95eeaa3f861e7293217dbb102e8c2ba
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-10-17 20:07:16 +0530

    feat: upgraded stripe api-version

--- packages/nrpc/src/config/stripe.ts
+++ packages/nrpc/src/config/stripe.ts
@@ -3,5 +3,5 @@ import Stripe from "stripe"
 
 export const stripe = new Stripe(ENV.STRIPE_SECRET_KEY, {
   typescript: true,
-  apiVersion: "2025-08-27.basil",
+  apiVersion: "2025-09-30.clover",
 })
