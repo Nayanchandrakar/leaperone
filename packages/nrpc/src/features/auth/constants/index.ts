e08f2c6522209da2eed7e6917d3f30c03e264ca4
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-10-18 13:42:03 +0530

    fix: cookie and cors issue for dev and prod:1

--- packages/nrpc/src/features/auth/constants/index.ts
+++ packages/nrpc/src/features/auth/constants/index.ts
@@ -1,4 +1,5 @@
 import { SESSION_EXPIRY } from "@app/core/constants"
+import { ENV } from "@app/env/server"
 import type { CookieOptions } from "hono/utils/cookie"
 import { getDate } from "../../../utils/date"
 
@@ -8,4 +9,5 @@ export const COOKIE_OPTIONS: CookieOptions = {
   httpOnly: true,
   sameSite: "Lax",
   expires: getDate(SESSION_EXPIRY, "sec"),
+  domain: new URL(ENV.FRONTEND_URL)?.hostname,
 } as const
