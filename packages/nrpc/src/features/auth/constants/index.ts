462ec5fd2ebe17c044909a144365e9cb1369ae8c
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-10-18 15:42:10 +0530

    fix: subdomain cookie support for both prod and dev environments

--- packages/nrpc/src/features/auth/constants/index.ts
+++ packages/nrpc/src/features/auth/constants/index.ts
@@ -1,6 +1,10 @@
 import { SESSION_EXPIRY } from "@app/core/constants"
+import { ENV } from "@app/env/server"
 import type { CookieOptions } from "hono/utils/cookie"
 import { getDate } from "../../../utils/date"
+import { getRootDomain } from "../../../utils/domain"
+
+const domain = getRootDomain(ENV.FRONTEND_URL)
 
 export const COOKIE_OPTIONS: CookieOptions = {
   path: "/",
@@ -8,4 +12,5 @@ export const COOKIE_OPTIONS: CookieOptions = {
   httpOnly: true,
   sameSite: "Lax",
   expires: getDate(SESSION_EXPIRY, "sec"),
+  ...(domain && { domain }),
 } as const
