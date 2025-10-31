7f699f0a79136e5e60783c98d795c6758ebab015
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-10-31 21:23:49 +0530

    Revert "fix: apps/server deployment issue with vercel resolve typescript"
    This reverts commit 9486815a8ce59b994c37f7b23b4cf5e350764164.

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,5 +1,4 @@
 //  biome-ignore-all lint/correctness/noUnusedImports: vercel deployment
-// @ts-expect-error
 import { Hono } from "hono"
 import { BootStrap } from "@/bootstrap"
 import { assetController } from "@/features/asset/modules/asset.module"
@@ -14,7 +13,7 @@ const server = new BootStrap([
   subscriptionController,
 ])
 
-// server.listen()
+server.listen()
 
 // Exporting the instance for vercel deployment
 export default server.instance
