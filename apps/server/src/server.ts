cbce0e661e92a6ff58a76a1e9b2d90663533f7fa
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-10-31 21:14:49 +0530

    fix: apps/server deployment issue with vercel

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -13,7 +13,7 @@ const server = new BootStrap([
   subscriptionController,
 ])
 
-server.listen()
+// server.listen()
 
 // Exporting the instance for vercel deployment
 export default server.instance
