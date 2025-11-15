3ac320d657f8fd8c83b7b3f260abfbedc3201bd6
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-11-15 14:25:49 +0530

    fix: enable permission check for now in asset-manager

--- apps/web/app/(dashboard)/[workspaceId]/(routes)/asset-manager/page.tsx
+++ apps/web/app/(dashboard)/[workspaceId]/(routes)/asset-manager/page.tsx
@@ -1,3 +1,4 @@
+import { DashboardPipeline } from "@/features/dashboard/actions/dashboard-pipeline"
 import { FilePreviewDialog } from "@/features/dashboard/components/dialogs/asset-manager/file-preview"
 import { Filters } from "@/features/dashboard/components/pages/asset-manager/filters"
 import { RenderFiles } from "@/features/dashboard/components/pages/asset-manager/render-files"
@@ -5,13 +6,15 @@ import { Toolbar } from "@/features/dashboard/components/pages/asset-manager/too
 import { DashboardContainer } from "@/features/dashboard/components/ui/dashboard-container"
 import { DashboardTitle } from "@/features/dashboard/components/ui/dashboard-heading"
 
-// import { DashboardPipeline } from "@/features/dashboard/actions/dashboard-pipeline"
+interface Props {
+  params: Promise<{ workspaceId: string }>
+}
 
-export default function AssetManagerPage() {
-  // const pipeline = await DashboardPipeline.init(params)
-  // await pipeline.checkMembership()
-  // await pipeline.checkPermissions(["manage:members"])
-  // await pipeline.checkSubscription()
+export default async function AssetManagerPage({ params }: Props) {
+  const pipeline = await DashboardPipeline.init(params)
+  await pipeline.checkMembership()
+  await pipeline.checkPermissions(["manage:members"])
+  await pipeline.checkSubscription()
 
   return (
     <DashboardContainer>
