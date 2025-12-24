2292d4830bc21d5ea958ed616f4d062629d3da8b
Nayanchandrakar <chandrakarnayan96@gmail.com>
2025-12-24 16:24:26 +0530

    chore: update Tailwind CSS and related packages to version 4.1.18

--- apps/web/app/preview/page.tsx
+++ apps/web/app/preview/page.tsx
@@ -7,22 +7,25 @@ import Image from "next/image"
 export default function PreviewPage() {
   return (
     <section className="max-w-[430px] mx-auto my-24 bg-muted p-4 space-y-5">
-      {/* about me section */}
-      <section className="flex-center flex-col gap-2 bg-white p-7 rounded-3xl">
-        <h3 className="text-[28px] font-semibold text-primary">About Me</h3>
+      {/* about me section complete */}
+      <section className="space-y-3 bg-white p-8 pt-7 rounded-3xl">
+        <h3 className="text-[28px] text-center font-semibold text-primary">About Me</h3>
         <p className="text-base font-normal text-muted-foreground text-center">
           At Electrica, I help people find the right EV cars for their lifestyle. I’m here to make
           the process of owning your dream EV hassle-free, transparent, and genuinely enjoyable.
         </p>
       </section>
 
-      {/* Our EV collection section */}
-      <section className="flex-center flex-col gap-2 bg-white p-5 rounded-3xl">
-        <h3 className="text-[28px] font-semibold text-primary">Our EV Collection</h3>
-        <p className="text-base font-normal text-muted-foreground text-center">
-          Discover our latest collection of electric vehicles from leading EV brands.
-        </p>
-        <div className="flex flex-col gap-4 mt-3">
+      {/* Our EV collection section complete */}
+      <section className="space-y-6 bg-white pt-7 px-4 pb-4 rounded-3xl">
+        <div className="space-y-3">
+          <h3 className="text-[28px] text-center font-semibold text-primary">Our EV Collection</h3>
+          <p className="text-base font-normal text-muted-foreground text-center">
+            Discover our latest collection of electric vehicles from leading EV brands.
+          </p>
+        </div>
+
+        <div className="grid gap-4">
           <Image
             width={1000}
             height={1000}
@@ -52,13 +55,16 @@ export default function PreviewPage() {
         </div>
       </section>
 
-      {/* Our EV collection section */}
-      <section className="flex-center flex-col gap-2 bg-white p-5 rounded-3xl">
-        <h3 className="text-[28px] font-semibold text-primary">Our EV Collection</h3>
-        <p className="text-base font-normal text-muted-foreground text-center">
-          Discover our latest collection of electric vehicles from leading EV brands.
-        </p>
-        <div className="grid grid-cols-2 gap-4 mt-3">
+      {/* Our EV collection section complete */}
+      <section className="space-y-6 bg-white pt-7 px-4 pb-4 rounded-3xl">
+        <div className="space-y-3">
+          <h3 className="text-[28px] text-center font-semibold text-primary">Our EV Collection</h3>
+          <p className="text-base font-normal text-muted-foreground text-center">
+            Discover our latest collection of electric vehicles from leading EV brands.
+          </p>
+        </div>
+
+        <div className="grid grid-cols-2 gap-4">
           <Image
             width={1000}
             height={1000}
@@ -95,13 +101,16 @@ export default function PreviewPage() {
         </div>
       </section>
 
-      {/* Our EV collection section */}
-      <section className="flex-center flex-col gap-2 bg-white p-5 rounded-3xl">
-        <h3 className="text-[28px] font-semibold text-primary">Our EV Collection</h3>
-        <p className="text-base font-normal text-muted-foreground text-center">
-          Discover our latest collection of electric vehicles from leading EV brands.
-        </p>
-        <div className="grid grid-cols-2 gap-4 mt-3">
+      {/* Our EV collection section complete */}
+      <section className="space-y-6 bg-white pt-7 px-4 pb-4 rounded-3xl">
+        <div className="space-y-3">
+          <h3 className="text-[28px] text-center font-semibold text-primary">Our EV Collection</h3>
+          <p className="text-base font-normal text-muted-foreground text-center">
+            Discover our latest collection of electric vehicles from leading EV brands.
+          </p>
+        </div>
+
+        <div className="grid grid-cols-2 gap-4">
           <Image
             width={1000}
             height={1000}
@@ -129,9 +138,9 @@ export default function PreviewPage() {
         </div>
       </section>
 
-      {/* Get in touch section */}
-      <section className="flex-center flex-col gap-4 bg-white p-5 rounded-3xl">
-        <h3 className="text-[28px] font-semibold text-primary">Get in touch</h3>
+      {/* Get in touch section complete */}
+      <section className="space-y-4 bg-white p-8 rounded-3xl">
+        <h3 className="text-[28px] font-semibold text-center text-primary">Get in touch</h3>
 
         <div className="w-full space-y-3">
           <div className="text-start font-normal text-base">
@@ -161,9 +170,9 @@ export default function PreviewPage() {
         </div>
       </section>
 
-      {/* Connect with me section */}
-      <section className="flex-center flex-col gap-4 bg-white p-5 rounded-3xl">
-        <h3 className="text-[28px] font-semibold text-primary">Connect with me</h3>
+      {/* Connect with me section complete */}
+      <section className="space-y-6 bg-white py-9 px-10 rounded-3xl">
+        <h3 className="text-[28px] text-center font-semibold text-primary">Connect with me</h3>
 
         <div className="w-full">
           <div className="flex items-center justify-between gap-2 py-3 border-b border-border">
@@ -173,6 +182,7 @@ export default function PreviewPage() {
               </span>
               <p className="font-normal text-base text-muted-foreground">Facebook</p>
             </div>
+
             <ArrowUpRight className="size-5 text-muted-foreground" />
           </div>
 
@@ -198,45 +208,61 @@ export default function PreviewPage() {
         </div>
       </section>
 
-      {/* Button section */}
-      <section className="flex-center flex-col gap-4 bg-white p-5 rounded-3xl">
-        <h3 className="text-[28px] font-semibold text-primary">Button section</h3>
-        <p className="text-base text-center font-normal text-muted-foreground">
-          Add a description or click on eye icon to hide this.
-        </p>
-        <Button className="w-full">Visit Website</Button>
+      {/* Button section complete */}
+      <section className="space-y-6 bg-white p-8 pt-7 rounded-3xl">
+        <div className="space-y-3">
+          <h3 className="text-[28px] text-center font-semibold text-primary">Button section</h3>
+          <p className="text-base text-center font-normal text-muted-foreground">
+            Add a description or click on eye icon to hide this.
+          </p>
+        </div>
+        <Button size="lg" className="w-full">
+          Visit Website
+        </Button>
       </section>
 
-      {/* Button section */}
-      <section className="flex-center flex-col gap-4 bg-white p-5 rounded-3xl">
-        <h3 className="text-[28px] font-semibold text-primary">Meet the team</h3>
-        <p className="text-base text-center font-normal text-muted-foreground">
-          Add a description or click on eye icon to hide this.
-        </p>
+      {/* Testimonial section complete */}
+      <section className="space-y-2">
+        <div className="space-y-3 bg-white py-7 px-8 rounded-3xl">
+          <h3 className="text-[28px] text-center font-semibold text-primary">Meet the team</h3>
+          <p className="text-base text-center font-normal text-muted-foreground">
+            Add a description or click on eye icon to hide this.
+          </p>
+        </div>
+
+        {Array.from({ length: 5 }).map((_, index) => (
+          <div key={index} className="py-6 px-8 rounded-3xl bg-white flex items-center gap-4">
+            <Avatar className="size-12">
+              <AvatarImage src={faker.image.avatar()} />
+            </Avatar>
+
+            <div className="-space-y-0.5">
+              <span className="font-semibold text-sm text-muted-foreground">
+                {faker.person.firstName()}
+              </span>
+              <p className="font-normal text-xs text-muted-foreground">{faker.person.jobTitle()}</p>
+            </div>
+          </div>
+        ))}
       </section>
 
-      {/*  Testimonial card section */}
-      <div className="p-5 rounded-3xl bg-white flex items-center gap-4">
-        <Avatar className="size-12">
-          <AvatarImage src={faker.image.avatar()} />
-        </Avatar>
-
-        <div>
-          <span className="font-semibold text-sm text-muted-foreground">Portia M.</span>
-          <p className="font-normal text-xs text-muted-foreground">Designation, Company</p>
+      {/* Youtube video section complete */}
+      <section className="bg-white rounded-3xl overflow-hidden">
+        <div className="space-y-3 py-7 px-8">
+          <h3 className="text-[28px] text-center font-semibold text-primary">Video</h3>
+          <p className="text-base text-center font-normal text-muted-foreground">
+            Add a description or click on eye icon to hide this.
+          </p>
         </div>
-      </div>
 
-      <div className="p-5 rounded-3xl bg-white flex items-center gap-4">
-        <Avatar className="size-12">
-          <AvatarImage src={faker.image.avatar()} />
-        </Avatar>
-
-        <div>
-          <span className="font-semibold text-sm text-muted-foreground">David Ortiz</span>
-          <p className="font-normal text-xs text-muted-foreground">Designation, Company</p>
-        </div>
-      </div>
+        <iframe
+          allowFullScreen
+          title="YouTube video player"
+          className="w-full rounded-b-3xl aspect-video"
+          allow="accelerometer; autoplay; encrypted-media;"
+          src="https://www.youtube-nocookie.com/embed/mfv0V1SxbNA?si=TFysOtlLt1XiFrFs"
+        />
+      </section>
     </section>
   )
 }
