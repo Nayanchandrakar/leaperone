e13caf6398297b3a2d0abf50bc8198a52bbb683f
Nayanchandrakar <chandrakarnayan96@gmail.com>
2026-01-06 11:27:00 +0530

    Revert "feat: Add invitation module with controller and messages for managing invitations"
    This reverts commit b816604d0322afbca0ced5f4928284f2d6b96e91.

--- apps/server/src/features/invitation/modules/invitation.module.ts
+++
@@ -1,7 +0,0 @@
-import { InvitationController } from "@/features/invitation/controllers/invitation.controller"
-import { InvitationService } from "@/features/invitation/services/invitation.service"
-
-const invitationService = new InvitationService()
-const invitationController = new InvitationController(invitationService)
-
-export { invitationService, invitationController }
