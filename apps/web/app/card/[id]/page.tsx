fe6c355c32ad023ecbd19b202a84fc972a2b9ffd
Nayanchandrakar <chandrakarnayan96@gmail.com>
2026-02-22 22:16:18 +0530

    feat: update QR code card preview to utilize content and design editor stores; enhance logging for better debugging and modify template renderer to disable server-side rendering

---
+++ apps/web/app/card/[id]/page.tsx
@@ -0,0 +1,56 @@
+import { dbHttp } from "@app/database/adapters/http"
+import { businessCard, subscription } from "@app/database/schema"
+import { eq } from "drizzle-orm"
+import { notFound } from "next/navigation"
+import { TemplateRenderer } from "@/features/preview/components/ui/template-renderer"
+import type { ContentSections } from "@/features/preview/types"
+
+export const runtime = "edge"
+
+type PageProps = {
+  params: Promise<{
+    id: string
+  }>
+}
+
+export default async function CardPage({ params }: PageProps) {
+  const { id } = await params
+
+  // Fetch the business card data by id
+  const [data] = await dbHttp
+    .select({
+      businessCard: businessCard,
+      subscription,
+    })
+    .from(businessCard)
+    .where(eq(businessCard.id, id))
+    .innerJoin(subscription, eq(businessCard.workspaceId, subscription.workspaceId))
+    .limit(1)
+
+  if (!data) {
+    return notFound()
+  }
+
+  // Expecting data.content to conform to ContentEditor type,
+  // which is not the same as the expected array of ContentSections.
+  // Here we try to extract relevant fields.
+
+  const {
+    businessCard: { design, template, status },
+  } = data
+
+  if (status !== "active") {
+    return <p className="text-center text-sm text-muted-foreground">This card is not active</p>
+  }
+
+  const sections = data.businessCard.content as unknown as ContentSections[]
+
+  return (
+    <TemplateRenderer
+      design={design}
+      contents={sections}
+      template={template}
+      floating={undefined}
+    />
+  )
+}
--- apps/web/features/bussiness/components/preview/home/qr-code-card-preview.tsx
+++ apps/web/features/bussiness/components/preview/home/qr-code-card-preview.tsx
@@ -1,11 +1,22 @@
 import { Button } from "@app/ui/components/button"
+import { useShallow } from "zustand/react/shallow"
 import { QrCodePreview, QrCodeProvider } from "@/features/bussiness/components/ui/qr-code"
+import { useContentEditorStore } from "@/features/bussiness/stores/use-content-editor-store"
+import { useDesignEditorStore } from "@/features/bussiness/stores/use-design-editor-store"
 import { useQrCodeEditorStore } from "@/features/bussiness/stores/use-qr-code-editor-store"
 
 export default function QrCodeCardPreview() {
+  const content = useContentEditorStore(
+    useShallow((state) => ({
+      template: state.template,
+      sections: state.sections,
+    })),
+  )
+
+  const design = useDesignEditorStore((state) => state.config)
   const options = useQrCodeEditorStore((state) => state.settings)
 
-  console.log(options)
+  console.log(content, design)
 
   return (
     <div className="space-y-4">
